20a21d97427b8eefe7e8913a01ad1ab0eb67c856
Ben Ebel <bge1234@gmail.com>
2016-04-08 11:32:52 -0600

    got rid of references to projects

--- todo.md
+++ todo.md
@@ -1,4 +1,7 @@
-- F: Remove "projects" portions and save prefs directly. Write matching algorithm (output results as list), then make it Tinder style with cards.
-- Weekend: Catch up if behind.
-- M: Clean up CSS/look and feel (more like a phone). Use ng-cloak.
-- T: Implement learning of user preferences based on input (stretch goal).
+- F/weekend:
+  Remove "projects" portions and save prefs directly.
+  Make it so only one set of pref saves and gets overwritten with new ones.
+  Write matching algorithm (output results as list). Triggered/retriggered by a button.
+
+- M: Clean up CSS/look and feel (more like a phone). Use ng-cloak. Separation of concerns for Angular items.
+- T: Make it Tinder style with cards. Swipe right, it goes into a container to be reviewed later. Swipe left, and it's gone.
--- www/js/app.js
+++ www/js/app.js
@@ -1,82 +1,35 @@
 angular.module('starter', ['ionic'])
-/**
- * The Projects factory handles saving and loading projects
- * from local storage, and also lets us save and load the
- * last active project index.
- */
-.factory('Projects', function() {
+.factory('Prefs', function() {
   return {
     all: function() {
-      var projectString = window.localStorage['projects'];
-      if(projectString) {
-        return angular.fromJson(projectString);
+      var prefsString = window.localStorage['prefs'];
+      if(prefsString) {
+        return angular.fromJson(prefsString);
       }
       return [];
     },
-    save: function(projects) {
-      window.localStorage['projects'] = angular.toJson(projects);
+    save: function(prefs) {
+      window.localStorage['prefs'] = angular.toJson(prefs);
     },
-    newProject: function(projectTitle) {
-      // Add a new project
+    newPrefs: function() {
       return {
-        title: projectTitle,
-        set_of_prefs: []
+        prefs: []
       };
-    },
-    getLastActiveIndex: function() {
-      return 0;
-    },
-    setLastActiveIndex: function(index) {
-      window.localStorage['lastActiveProject'] = index;
     }
   }
 })
 
-.controller('TodoCtrl', function($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate) {
-
-  // A utility function for creating a new project
-  // with the given projectTitle
-  var createProject = function(projectTitle) {
-    var newProject = Projects.newProject(projectTitle);
-    $scope.projects.push(newProject);
-    Projects.save($scope.projects);
-    $scope.selectProject(newProject, $scope.projects.length-1);
-  }
-
-
-  // Load or initialize projects
-  $scope.projects = Projects.all();
-
-  // Grab the last active, or the first project
-  $scope.activeProject = $scope.projects[Projects.getLastActiveIndex()];
-
-  // Called to create a new project
-  $scope.newProject = function() {
-    var projectTitle = prompt('Project name');
-    if(projectTitle) {
-      createProject(projectTitle);
-    }
-  };
-
-  // Called to select the given project
-  $scope.selectProject = function(project, index) {
-    $scope.activeProject = project;
-    Projects.setLastActiveIndex(index);
-    $ionicSideMenuDelegate.toggleLeft(false);
-  };
-
-  // Create our modal
+.controller('TodoCtrl', function($scope, $timeout, $ionicModal, Prefs, $ionicSideMenuDelegate) {
   $ionicModal.fromTemplateUrl('new-prefs.html', function(modal) {
     $scope.prefsModal = modal;
   }, {
     scope: $scope
   });
 
+  $scope.set_of_prefs = Prefs.all();
+
   $scope.createPrefs = function(prefs) {
-    if(!$scope.activeProject || !prefs) {
-      return;
-    }
-    $scope.activeProject.set_of_prefs.push({
+    $scope.set_of_prefs.push({
       heatcold: prefs.heatcold,
       snow: prefs.snow,
       seasons: prefs.seasons,
@@ -92,10 +45,6 @@ angular.module('starter', ['ionic'])
       occupation: prefs.occupation
     });
     $scope.prefsModal.hide();
-
-    // Inefficient, but save all the projects
-    Projects.save($scope.projects);
-
   };
 
   $scope.newPrefs = function() {
@@ -105,25 +54,4 @@ angular.module('starter', ['ionic'])
   $scope.closeNewPrefs = function() {
     $scope.prefsModal.hide();
   }
-
-  $scope.toggleProjects = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
-
-
-  // Try to create the first project, make sure to defer
-  // this by using $timeout so everything is initialized
-  // properly
-  $timeout(function() {
-    if($scope.projects.length == 0) {
-      while(true) {
-        var projectTitle = prompt('Your first project title:');
-        if(projectTitle) {
-          createProject(projectTitle);
-          break;
-        }
-      }
-    }
-  });
-
 });
