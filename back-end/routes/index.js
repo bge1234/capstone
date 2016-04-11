8483618debd71bf493addb7a9ef644e626487423
Ben Ebel <bge1234@gmail.com>
2016-04-11 14:42:08 -0600

    got all API calls working in back end except the one that uses headers

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -70,78 +70,72 @@ router.get('/makecalls', function(req, res, next) {
               console.log(body);
               console.log(body["cpi_index"]);
               output[0]["data"]["cpi"] += 100;
-              res.json(output);
-            }
-          });
 
-          // Elevation
-          request("http://ned.usgs.gov/epqs/pqs.php?x=" + output[0]["data"]["location"]["longitude"] + "&y=" + output[0]["data"]["location"]["latitude"] + "&units=Feet&output=json", function (error, response, body) {
-            if (error) {
-              console.log("Error!  Request failed - " + error);
-            } else if (!error && response.statusCode === 200) {
-              console.log(body);
-              console.log(body["cpi_index"]);
-              output[0]["data"]["cpi"] += 100;
-              res.json(output);
-            }
-          });
-
-          // Trails
-          // headers: {"X-Mashape-Key": "vCAJivs8Hbmshar7q8mbiQyvzgxtp196VFJjsnU83v4lteo5fQ"}
-          request("https://trailapi-trailapi.p.mashape.com/?q[city_cont]=" + output[i]["city"] + "&q[country_cont]=United+States&q[state_cont]=" + output[i]["data"]["location"]["state"] + "&radius=5", function (error, response, body) {
-            if (error) {
-              console.log("Error!  Request failed - " + error);
-            } else if (!error && response.statusCode === 200) {
-              console.log(body);
-              console.log(body["cpi_index"]);
-              output[0]["data"]["cpi"] += 100;
-              res.json(output);
-            }
-          });
-
-          // Parks
-          request("https://maps.googleapis.com/maps/api/place/textsearch/json?query=parks+in+" + output[0]["city"] + "&key=AIzaSyBI9s3tzAqmyq24EaLFF0E9h-Up7Eo1A3w", function (error, response, body) {
-            if (error) {
-              console.log("Error!  Request failed - " + error);
-            } else if (!error && response.statusCode === 200) {
-              console.log(body);
-              console.log(body["cpi_index"]);
-              output[0]["data"]["cpi"] += 100;
-              res.json(output);
-            }
-          });
-
-          // Museums
-          request("https://maps.googleapis.com/maps/api/place/textsearch/json?query=museums+in+" + output[0]["city"] + "&key=AIzaSyBI9s3tzAqmyq24EaLFF0E9h-Up7Eo1A3w", function (error, response, body) {
-            if (error) {
-              console.log("Error!  Request failed - " + error);
-            } else if (!error && response.statusCode === 200) {
-              console.log(body);
-              console.log(body["cpi_index"]);
-              output[0]["data"]["cpi"] += 100;
-              res.json(output);
-            }
-          });
-
-          //********
-
-          // Universities
-          request("https://maps.googleapis.com/maps/api/place/textsearch/json?query=universities+in+" + output[0]["city"] + "&key=AIzaSyBI9s3tzAqmyq24EaLFF0E9h-Up7Eo1A3w", function (error, response, body) {
-            if (error) {
-              console.log("Error!  Request failed - " + error);
-            } else if (!error && response.statusCode === 200) {
-              console.log(body);
-              output[0]["data"]["cpi"] += 100;
-
-              //Cost of living
-              request("http://www.numbeo.com/api/indices?api_key=y8cjt0c06w8o3g&query=" + output[0]["city"], function (error, response, body) {
+              // Elevation
+              request("http://ned.usgs.gov/epqs/pqs.php?x=" + output[0]["data"]["location"]["longitude"] + "&y=" + output[0]["data"]["location"]["latitude"] + "&units=Feet&output=json", function (error, response, body) {
                 if (error) {
                   console.log("Error!  Request failed - " + error);
                 } else if (!error && response.statusCode === 200) {
                   console.log(body);
                   console.log(body["cpi_index"]);
                   output[0]["data"]["cpi"] += 100;
-                  res.json(output);
+
+                  // Trails
+                  // headers: {"X-Mashape-Key": "vCAJivs8Hbmshar7q8mbiQyvzgxtp196VFJjsnU83v4lteo5fQ"}
+                  // request("https://trailapi-trailapi.p.mashape.com/?q[city_cont]=" + output[i]["city"] + "&q[country_cont]=United+States&q[state_cont]=" + output[i]["data"]["location"]["state"] + "&radius=5", function (error, response, body) {
+                  request("http://ned.usgs.gov/epqs/pqs.php?x=" + output[0]["data"]["location"]["longitude"] + "&y=" + output[0]["data"]["location"]["latitude"] + "&units=Feet&output=json", function (error, response, body) {
+                    if (error) {
+                      console.log("Error!  Request failed - " + error);
+                    } else if (!error && response.statusCode === 200) {
+                      console.log(body);
+                      console.log(body["cpi_index"]);
+                      output[0]["data"]["cpi"] += 100;
+
+                      // Parks
+                      request("https://maps.googleapis.com/maps/api/place/textsearch/json?query=parks+in+" + output[0]["city"] + "&key=AIzaSyBI9s3tzAqmyq24EaLFF0E9h-Up7Eo1A3w", function (error, response, body) {
+                        if (error) {
+                          console.log("Error!  Request failed - " + error);
+                        } else if (!error && response.statusCode === 200) {
+                          console.log(body);
+                          console.log(body["cpi_index"]);
+                          output[0]["data"]["cpi"] += 100;
+
+                          // Museums
+                          request("https://maps.googleapis.com/maps/api/place/textsearch/json?query=museums+in+" + output[0]["city"] + "&key=AIzaSyBI9s3tzAqmyq24EaLFF0E9h-Up7Eo1A3w", function (error, response, body) {
+                            if (error) {
+                              console.log("Error!  Request failed - " + error);
+                            } else if (!error && response.statusCode === 200) {
+                              console.log(body);
+                              console.log(body["cpi_index"]);
+                              output[0]["data"]["cpi"] += 100;
+
+                              // Universities
+                              request("https://maps.googleapis.com/maps/api/place/textsearch/json?query=universities+in+" + output[0]["city"] + "&key=AIzaSyBI9s3tzAqmyq24EaLFF0E9h-Up7Eo1A3w", function (error, response, body) {
+                                if (error) {
+                                  console.log("Error!  Request failed - " + error);
+                                } else if (!error && response.statusCode === 200) {
+                                  console.log(body);
+                                  output[0]["data"]["cpi"] += 100;
+
+                                  //Cost of living
+                                  request("http://www.numbeo.com/api/indices?api_key=y8cjt0c06w8o3g&query=" + output[0]["city"], function (error, response, body) {
+                                    if (error) {
+                                      console.log("Error!  Request failed - " + error);
+                                    } else if (!error && response.statusCode === 200) {
+                                      console.log(body);
+                                      console.log(body["cpi_index"]);
+                                      output[0]["data"]["cpi"] += 100;
+                                      res.json(output);
+                                    }
+                                  });
+                                }
+                              });
+                            }
+                          });
+                        }
+                      });
+                    }
+                  });
                 }
               });
             }
--- todo.md
+++ todo.md
@@ -1,8 +1,9 @@
-- Make API calls and save data to output JSON.
-- Make sure CORS isn't an issue in both Chrome and iPhone emulator. Fix it if it is.
+- Replace output[0]["data"]["cpi"] += 100; with setting actual property equal to actual data (need Nmuta's help first).
+- Get for loop working again (in order to do all cities dynamically instead of just the first).
 - Copy JSON from back end into front end in case Heroku is a problem during presentation.
-- Consume JSON from back end and display in front end.
+- Call JSON from back end and display in front end.
 - Clean up API stuff and data in front end that were moved to back end.
+
 - In main controller, iterate through my JSON object to match data against user prefs. Use separate function for each category, then use one master function to call them all.
 
 - Make it Tinder style with cards. On swipe right, it goes into a container to be reviewed later. Swipe left, and it's gone.
